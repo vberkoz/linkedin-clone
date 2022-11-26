ae46bc5ff2dc5fd18b5595ca351e618c83785c76
Vasyl Berkoz <vberkoz@gmail.com>
2022-11-26 08:11:38 +0200

    Building the Sidebar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
-    <div className="App">
-      {/* Header */}
+    <div className="app">
       <Header />
+
       {/* App Body */}
-      {/* Sidebar */}
-      {/* Feed */}
-      {/* Widgets */}
+      <div className="app__body">
+        <Sidebar />
+        {/* Feed */}
+        {/* Widgets */}
+      </div>
     </div>
   );
 }
---
+++ src/Sidebar.js
@@ -0,0 +1,48 @@
+import { Avatar } from "@mui/material";
+import React from "react";
+import "./Sidebar.css";
+
+function Sidebar() {
+  const recentItem = (topic) => (
+    <div className="sidebar__recentItem">
+      <span className="sideber__hash">#</span>
+      <p>{topic}</p>
+    </div>
+  );
+
+  return (
+    <div className="sidebar">
+      <div className="sidebar__top">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXgHyNAUTsr3BRyIHVo47tDBVKCE1CKNIhtf-RTRH6ldaPpuPEPptZuefSOftS-avuF9E&usqp=CAU"
+          alt=""
+        />
+        <Avatar className="sidebar__avatar" />
+        <h2>Sonny Sangha</h2>
+        <h4>sonny.sangha@gmail.com</h4>
+      </div>
+
+      <div className="sidebar__stats">
+        <div className="sidebar__stat">
+          <p>Who viewed you</p>
+          <p className="sidebar__statNumber">2,543</p>
+        </div>
+        <div className="sidebar__stat">
+          <p>Who viewed you</p>
+          <p className="sidebar__statNumber">2,543</p>
+        </div>
+      </div>
+
+      <div className="sidebar__bottom">
+        <p>Recent</p>
+        {recentItem("reactjs")}
+        {recentItem("programming")}
+        {recentItem("softwareengineering")}
+        {recentItem("design")}
+        {recentItem("developer")}
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
